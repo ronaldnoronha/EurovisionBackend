a0502552b863828a3234566c4b06906f31b5dcc9
Ronald Noronha <ronald.noronha@cba.com.au>
2023-04-14 22:25:51 +1000

    Initial commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,12 @@
 import Fluent
 import Vapor
 
+struct User: Content {
+    let name: String
+    let password: String
+}
+
+
 func routes(_ app: Application) throws {
     app.get { req async in
         "It works!"
@@ -9,6 +15,37 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    // /login?name=ron&password=hey
+    
+    app.get("login") { req async throws in
+        let user = try req.query.decode(User.self)
 
+        if user.name != "ron" {
+            throw Abort(.badRequest, reason: "No user found")
+        }
+
+        if user.password != "hey" {
+            throw Abort(.badRequest, reason: "Incorrect password")
+        }
+        
+        return "user \(user.name) and password \(user.password)"
+    }
+    
+    
+    // /signup
+    app.post("signup") { req async throws in
+        let user = try req.content.decode(User.self)
+        
+        // TODO: - check in database if the name already exists
+        if user.name == "harish" {
+            throw Abort(.badRequest, reason: "User already exists")
+        }
+        
+        // TODO: - Add to DB
+        
+        return HTTPStatus.ok
+    }
+    
     try app.register(collection: TodoController())
 }
